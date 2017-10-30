060ec45648179c2bd2d1aab1f292c9dcaef6b2bb
jag_k <jag-k58@ya.ru>
2017-10-30 19:21:31 +0300

    Добавлен ввод/вывод функции и комментарии к каждому параметру

--- select.py
+++ select.py
@@ -3,18 +3,28 @@
 # qpy:console
 
 
-def select(title="", *quest, numb=True, default=True, lang='eng'):
+def select(title="", *quest, numb=True, default=True, lang='eng', inpt=input, out=None):
+    """
+    :param title: Вопрос
+    :param quest: Варианты ответа
+    :param numb: Нумеровать ли вопросы?
+    :param default: Что будет если строка ввода будет пуста (если в quest пусто) (действие по умолчанию)?
+    :param lang: Язык (eng/rus)
+    :param inpt: Ввод
+    :param out: Вывод (по умолчанию в консоль)
+    :return: Bool (если в quest пусто) или (элемент quest, индекс элемента quest) (если в quest есть элементы)
+    """
     if quest:
-        print(title+':') if title else False
+        print(title+':', file=out) if title else False
         for i in range(len(quest)):
-            print('\t{}{}'.format(str(i+1)+') ' if numb else "", quest[i]+(';' if i != len(quest) else '.')))
+            print('\t{}{}'.format(str(i+1)+') ' if numb else "", quest[i]+(';' if i != len(quest) else '.')), file=out)
         while True:
-            t = input('1: ' if len(quest) == 1 else '1-'+str(len(quest))+': ')
+            t = inpt('1: ' if len(quest) == 1 else '1-'+str(len(quest))+': ')
             if t.isdigit():
                 if 0 < int(t) < len(quest)+1:
-                    return quest[int(t)-1]
+                    return quest[int(t)-1], int(t)-1
     else:
-        print(title, end=' ')
+        print(title, end=' ', file=out)
         if lang.lower() == 'eng' or lang.lower() == 'en':
             answer = '(Y/n)' if default else '(y/N)'
         elif lang.lower() == 'rus' or lang.lower() == 'ru':
@@ -22,7 +32,7 @@ def select(title="", *quest, numb=True, default=True, lang='eng'):
         else:
             answer = '(Y/n)' if default else '(y/N)'
         while True:
-            t = input(answer+': ')
+            t = inpt(answer+': ')
             if not t:
                 return default
             elif t:
@@ -31,6 +41,7 @@ def select(title="", *quest, numb=True, default=True, lang='eng'):
                     return True
                 elif l == 'n' or l == 'no' or l == 'н' or l == 'нет':
                     return False
+
 
 '''
 print(select('hate me?'))
