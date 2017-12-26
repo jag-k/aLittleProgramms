e83d002d648eb374eedc613773d799b0dd477702
Jag_K <jag-k58@ya.ru>
2017-12-26 16:13:27 +0300

    Небольшие изменения)

--- r_dictianory.py
+++ r_dictianory.py
@@ -1,6 +1,9 @@
+from pprint import pprint
+
+
 def r_dictionary(word):
     from urllib import request, parse
-
+    word = word.replace('?', '%3F')
     raw_data = str(request.urlopen('http://gramota.ru/slovari/dic/?word=%s&all=x' % parse.quote_plus(word)).read(),
                    encoding='windows-1251').split('</form>')[1].split('<div class="gray">')[0]
 
@@ -8,25 +11,58 @@ def r_dictionary(word):
                    ).rstrip('<div class="clear"></div><div class="gap-saver"></div></div>').split('<h2>')
     l = []
 
-    def format_replace(s: str):
-        s = s.replace('<br>', '\n').replace(' <b>&loz;</b> ', ' ◊ ').replace('<b>', '\x1b[1m')
-        s = s.replace('</b>', '\x1b[0m').replace('<i>', '').replace('</i>', '').replace('</div>', '')
-        s = s.replace('&mdash;', '—').replace('<span class="accent">', '\x1b[31;1m').replace('</span>', '\x1b[1m')
+    correct = True
+
+    def format_replace(s: str, decorate=True):
+        r = [
+            ('&mdash;', '—'),
+            ('&ndash', '–'),
+            (' <b>&loz;</b> ', ' ◊ '),
+            ('<b>', '\x1b[1m'),
+            ('<B>', '\x1b[1m'),
+            ('<i>', ''),
+            ('<I>', ''),
+            ('<br>', '\n'),
+            ('</b>', '\x1b[0m'),
+            ('</i>', ''),
+            ('</I>', ''),
+            ('</div>', ''),
+            ('<span class="accent">', '\x1b[31;1m'),
+            ('</span>', '\x1b[1m')
+        ]
+        for before, after in r:
+            s = s.replace(before, after if decorate else '')
         return s
 
     for i in filter(lambda x: x, data):
         dic = {}
-        if i.startswith('Синонимы: краткий справочник</h2>'):
-            dic['url'], name = None, i.replace('<h2>', '').replace('</h2>', '</a></h2>')
-        else:
-            dic['url'], name = i.replace('<a href="', '').split('/">')
+        if i.startswith('Искомое слово отсутствует'):
+            correct = False
+            continue
 
-        dic['name'], text = name.split('</a></h2><div style="padding-left:50px">')
+        elif not correct:
+            for j in i.split('Похожие слова:</h2>')[1].split('<p style="padding-left:50px">'):
+                dic = {}
+                url, text = j.split('<a href="')[1].split('&all=x">')
+                dic['url'] = url+'&all=x'
+                dic['decorated_text'] = format_replace(text)
+                dic['text'] = format_replace(text, False)
+                l.append(dic)
 
-        if text in ('искомое слово отсутствует</div>', 'искомое слово отсутствует'):
-            dic['text'] = None
         else:
-            dic['text'] = format_replace(text)
-        l.append(dic)
+
+            if i.startswith('Синонимы: краткий справочник</h2>'):
+                dic['url'], name = None, i.replace('<h2>', '').replace('</h2>', '</a></h2>')
+            else:
+                dic['url'], name = i.replace('<a href="', '').split('/">')
+
+            dic['name'], text = name.split('</a></h2><div style="padding-left:50px">')
+
+            if text.startswith('искомое слово отсутствует'):
+                dic['text'], dic['decorated_text'] = None, None
+            else:
+                dic['decorated_text'] = format_replace(text)
+                dic['text'] = format_replace(text, False)
+            l.append(dic)
 
     return l
